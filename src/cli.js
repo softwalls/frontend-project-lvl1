d6db9a1db343f9933ae413baf2b0fb7c20344e9f
Vyacheslav Nesmeyanov <softwalls@Vyacheslavs-Mac-mini.local>
2020-12-17 15:48:49 +0300

    add src directory and add cli.js

---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+export default function sayHey() {
+	const username = readlineSync.question('May I have your name, stranger?');
+	return (`Well, Hello, ${username}`);
+};
