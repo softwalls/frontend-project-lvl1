471cec712f0fbe765284f19cc1fc4f1bd1cbeaad
Vyacheslav Nesmeyanov <softwalls@Vyacheslavs-Mac-mini.local>
2020-12-29 02:29:53 +0300

    add another game (braincalc)

---
+++ bin/brain-calc.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { getRandomList, greetUser } from '../src/index.js';
+import brainCalc from '../src/braincalc.js';
+
+//   познакомимся, сгенерим
+const username = greetUser();
+console.log(`Hello, ${username}`);
+// сгенерим числа для раундов
+const numList = getRandomList(6);
+// зададим вопрос и запустим игру
+console.log('What is the result of the expression?');
+console.log(brainCalc(numList, username));
