c9137bf9a422e921458c82bb832ab625aec94487
Vyacheslav Nesmeyanov <softwalls@Vyacheslavs-Mac-mini.local>
2020-12-29 12:36:48 +0300

    add directory for games

---
+++ src/games/braincalc.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+
+const brainCalc = (numberList, username) => {
+  const operations = ['+', '-', '*'];
+
+  for (let i = 0; i < numberList.length; i += 2) {
+    const randomIndex = Math.floor(Math.random() * Math.floor(3));
+    const operand1 = numberList[i];
+    const operand2 = numberList[i + 1];
+    const operator = operations[randomIndex];
+    const answer = readlineSync.question(`*** Question: ${operand1} ${operator} ${operand2} `);
+    console.log(`Your answer: ${answer}`);
+    const userAnswer = Number(answer);
+    let correctAnswer;
+    switch (randomIndex) {
+      case 0:
+        correctAnswer = operand1 + operand2;
+        break;
+      case 1:
+        correctAnswer = operand1 - operand2;
+        break;
+      default:
+        correctAnswer = operand1 * operand2;
+    }
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      return `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${username}!`;
+    }
+  }
+  return `Congratulations, ${username}!`;
+};
+
+export default brainCalc;
---
+++ src/games/isEven.js
@@ -0,0 +1,17 @@
+import readlineSync from 'readline-sync';
+
+function isEven(list, username) {
+  for (let i = 0; i < list.length; i += 1) {
+    const userAnswer = readlineSync.question(`*** Question: ${list[i]} -> `);
+    const correctAnswer = (list[i] % 2 === 0) ? 'yes' : 'no';
+    console.log(`Your answer: ${userAnswer}`);
+    if (userAnswer === correctAnswer) {
+      console.log('correct');
+    } else {
+      return `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${username}!`;
+    }
+  }
+  return `Congratulations, ${username}!`;
+}
+
+export default isEven;
